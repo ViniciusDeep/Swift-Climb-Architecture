3d9b1ded84d2321228b14aafc9d88263a8e1cb42
Vinicius Mangueira <viniciuscorreia11@gmail.com>
2019-10-22 16:02:27 -0300

    [Launch] At example feature at see details at repositorie

---
+++ Example/Stalki/Stalki/App/Features/InsideRepo/View/InsideRepoHeaderView.swift
@@ -0,0 +1,9 @@
+//
+//  InsideRepoHeaderView.swift
+//  Stalki
+//
+//  Created by Vinicius Mangueira on 22/10/19.
+//  Copyright © 2019 Vinicius Mangueira. All rights reserved.
+//
+
+import Foundation
